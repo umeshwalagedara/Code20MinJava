f3188a2a502bb6219d7a116b4684b6169ab3d566
umeshwalagedara <umeshwalagedara@gmail.com>
2023-05-06 18:33:17 +0530

    Update FactorialNumber.java
    factorial find method added

--- src/com/umesh/may6/FactorialNumber.java
+++ src/com/umesh/may6/FactorialNumber.java
@@ -2,8 +2,21 @@ package com.umesh.may6;
 
 public class FactorialNumber {
 
+
+
   public static void main(String[] args) {
-    System.out.println(" testing ");
+
+    System.out.println(findFactorialOfNumber(3));
+  }
+
+
+  public static int findFactorialOfNumber(int number){
+    if(number == 0){
+      return 1;
+    }else{
+      return number * findFactorialOfNumber(number -1);
+    }
+
   }
 
 }
